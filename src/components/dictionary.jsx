71413c4cff743bed8b09df321d9d00887520d1e4
Denito37 <69406178+Denito37@users.noreply.github.com>
2023-03-12 13:25:49 -0400

    added info to dictionary, improve input handling

--- src/components/dictionary.jsx
+++ src/components/dictionary.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 
 export default function Dictionary(){
     const [word, setWord] = useState('...');
-    const [speech, setSpeech] = useState('')
-    const [secondSpeech, setSecondSpeech] = useState('')
+    const [pronounce, setPronounce] = useState('');
+    const [speech, setSpeech] = useState('');
+    const [secondSpeech, setSecondSpeech] = useState('');
     const [definition, setDefinition] = useState('...');
     const [secondDefinition, setSecondDefinition] = useState('...');
     const [example, setExample] = useState('...');
     const [secondExample, setSecondExample] = useState('...');
+    const [synonym, setSynonym] = useState('')
+    const [secondSynonym, setSecondSynonym] = useState('')
     const [input, setInput]= useState('hello');
 
     const getWord = async ()=>{
@@ -16,19 +19,24 @@ export default function Dictionary(){
             if(!obj.ok){throw new Error}
             let text = await obj.json();
             setWord(text[0].word);
+            setPronounce(text[0].phonetic);
             setSpeech(text[0].meanings[0].partOfSpeech)
             setDefinition(text[0].meanings[0].definitions[0].definition)
             setExample(text[0].meanings[0].definitions[0].example)
+            setSynonym(text[0].meanings[0].synonyms[0])
             setSecondSpeech(text[0].meanings[1].partOfSpeech)
             setSecondDefinition(text[0].meanings[1].definitions[0].definition)
             setSecondExample(text[0].meanings[1].definitions[0].example)
+            setSecondSynonym(text[0].meanings[1].synonyms[0])
         }
         catch(error){
             console.error(`error ${error}`)
             setSecondDefinition("not found")
             setSecondExample('not found')
+            setSecondSynonym('not found')
             // ! set error on what causes error ; currently sets 2nd def as "not found" if any of them have an error
         }
+        if(input === ''){setInput('hello')}
     }
 
     useEffect(() => {
@@ -40,14 +48,17 @@ export default function Dictionary(){
             <input onChange={event => setInput(event.target.value)} className=" bg-zinc-800 text-base px-8 py-6 rounded-2xl h-3/6 w-full  " type="text" placeholder="Search" />
             <section className="p-4 my-4 h-full">
                 <h2 className=" text-5xl capitalize">{word}</h2>
+                <p>{pronounce}</p>
                 <hr className="my-2" />
                 <small>({speech})</small>
                 <p className ="my-2"><small className=" font-bold text-slate-300">Definition: </small>{definition}</p>
                 <p className ="my-2"> <small className=" font-bold text-slate-300">Example: </small>{example}</p>
+                <p className ="my-2"> <small className=" font-bold text-slate-300">Synonym: </small>{synonym}</p>
                 <hr className=" my-2" />
                 <small>({secondSpeech})</small>
                 <p className ="my-2"><small className=" font-bold text-slate-300">Definition: </small>{secondDefinition}</p>
                 <p className ="my-2"> <small className=" font-bold text-slate-300">Example: </small>{secondExample}</p>
+                <p className ="my-2"> <small className=" font-bold text-slate-300">Synonym: </small>{secondSynonym}</p>
             </section>
         </article>
     )
--- src/components/zfooter.jsx
+++ src/components/zfooter.jsx
@@ -1,8 +1,9 @@
 export default function Footer(){
     return(
         <>
-            <footer>
-                <h2 className="text-xl text-center p-4">Coded by: <a href="https://denito37.github.io/wdp/" target="_blank" rel="noopener noreferrer">Me</a> </h2>
+            <footer className="p-4">
+                <h2 className="text-lg text-center p-1">Coded by: <a href="https://denito37.github.io/wdp/" target="_blank" rel="noopener noreferrer">Me</a> </h2>
+                <h2 className="text-lg text-center p-1"><a href="https://github.com/Denito37" target="_blank" rel="noopener noreferrer">Github</a></h2>
             </footer>
         </>
     )
